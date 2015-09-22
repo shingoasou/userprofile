ebdf100e0491ac9a2686a49dbf6623098019b5d0
shingoasou <s.a_agribrain@icloud.com>
2015-09-22 13:08:59 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'users#new'
+  root to: 'sessions#new'
   get 'signup',  to: 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
